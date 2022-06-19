6ea4f18b89a1369c87303c37f46f8ba3ec9900f7
yuhui.wang02 <yuhui.wang02@weimob.com>
2022-06-19 20:39:51 +0800

    反射示例

--- JavaLeakfilling/JavaBasic/src/main/java/com/ledolom/JavaBasic/Main.java
+++ JavaLeakfilling/JavaBasic/src/main/java/com/ledolom/JavaBasic/Main.java
@@ -1,9 +1,29 @@
 package com.ledolom.JavaBasic;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
-    public static void main(String[] args) {
-        Integer code = UseEnum.NO.getCode();
+    public static void main(String[] args) throws NoSuchMethodException, NoSuchFieldException, IllegalAccessException, InvocationTargetException {
+      /*  Integer code = UseEnum.NO.getCode();
         System.out.println(code);
-        System.out.println(UseEnum.valueOf("YES"));
+        System.out.println(UseEnum.valueOf("YES"));*/
+        User user = new User();
+        user.setName("三");
+        user.setAge(4);
+        Class<? extends User> aClass = user.getClass();
+        System.out.println(aClass);
+        Method next = aClass.getMethod("next", String.class);
+        next.invoke(user,"huo");
+        //反射调用静态方法
+        Method hai = aClass.getMethod("hai", Integer.class);
+        Object invoke = hai.invoke(null, 1);
+        System.out.println(invoke);
+
+
+        Field age = aClass.getDeclaredField("age");
+        age.setAccessible(true);
+        System.out.println(age.get(user));
     }
 }
---
+++ JavaLeakfilling/JavaBasic/src/main/java/com/ledolom/JavaBasic/User.java
@@ -0,0 +1,30 @@
+package com.ledolom.JavaBasic;
+
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void next(String name){
+        System.out.println(name);
+    }
+
+    public static void hai(Integer i){
+        System.out.println(i);
+    }
+}
