b8dde4f6276f5937d4c7338529bf6dffb60c9f49
yuhui.wang02 <yuhui.wang02@weimob.com>
2022-06-23 14:10:43 +0800

    添加线上版本json自动生成

--- JavaLeakfilling/JavaBasic/src/main/java/com/ledolom/JavaBasic/Main.java
+++ JavaLeakfilling/JavaBasic/src/main/java/com/ledolom/JavaBasic/Main.java
@@ -1,15 +1,19 @@
 package com.ledolom.JavaBasic;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws NoSuchMethodException, NoSuchFieldException, IllegalAccessException, InvocationTargetException {
       /*  Integer code = UseEnum.NO.getCode();
         System.out.println(code);
         System.out.println(UseEnum.valueOf("YES"));*/
-        User user = new User();
+       /* User user = new User();
         user.setName("三");
         user.setAge(4);
         Class<? extends User> aClass = user.getClass();
@@ -24,6 +28,12 @@ public class Main {
 
         Field age = aClass.getDeclaredField("age");
         age.setAccessible(true);
-        System.out.println(age.get(user));
+        System.out.println(age.get(user));*/
+        List<String> list = new ArrayList<>();
+        list.add("string");
+        list.add("string1");
+        list.add("string2");
+        String s = JSONObject.toJSONString(list);
+        System.out.println(s);
     }
 }
